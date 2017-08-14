d41bab8b02e4634c905255f3b168259c7a74168f
Nicolas Hodicq <nhodicq@bewizyu.com>
2017-08-14 11:20:17 +0200

    Init npm / eslint / express

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const app = express();
+
+/**
+ * CORS configurations
+ */
+app.all('/*', (req, res, next) => {
+  // CORS headers
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
+/**
+ * Hello world
+ */
+app.get('/hello', (req, res) => {
+  res.send('Hello test!');
+});
+
+/**
+ * Undefined routes return 404 http code
+ */
+app.use((req, res) => {
+  res.status(404).send({
+    message: 'URL not found',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
